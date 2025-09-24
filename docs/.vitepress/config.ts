d87d523cdeb0ab5b1cd097187322171bd6df798f
LiuYu <69404608+syskuku@users.noreply.github.com>
2025-09-24 11:50:29 +0800

    config.ts の更新

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -13,7 +13,7 @@ import llmstxt from 'vitepress-plugin-llms'
 const prod = !!process.env.NETLIFY
 
 export default defineConfig({
-  title: 'VitePress',
+  title: '高中数学',
 
   rewrites: {
     'en/:rest*': ':rest*'
